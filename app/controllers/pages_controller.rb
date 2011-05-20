68d7647997e65648b66c6980c03c14602e429e0a
brook <brook_realize@126.com>
2011-05-20 17:48:13 +0800

    classes and layouts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,17 @@
 class PagesController < ApplicationController
   def home
+    @title = "home"
   end
 
   def contact
+    @title = "Contact"
   end
   
   def about
+    @title = "About"
+  end
+  
+  def help
+    @title="Help"
   end
 end
