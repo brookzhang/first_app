1bda62ed1beb15a7f3fbbc8621b373dbc17b98d4
brook <brook_realize@126.com>
2011-05-24 16:55:14 +0800

    email TDD

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,4 +19,25 @@ describe User do
     no_email_user.should_not be_valid
   end
   
+  it "should reject names that are too long" do
+    long_name = "a" * 51
+    long_name_user = User.new(@attr.merge(:name=>long_name))
+    long_name_user.should_not be_valid
+  end
+  
+  it "should accept valid email address"do
+    addresses = %w[user@fo.com THE_USER@foo.bar.org first.last@foo.cn]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(:email=>address))
+      valid_email_user.should be_valid
+    end
+  end
+  
+  it "should reject invalid email addresses" do
+    addresses = %w[user@ff,com user_at.org emg.rr@ff.]
+    addresses.each do |address|
+      invalid_email_user = User.new(@attr.merge(:email=>address))
+      invalid_email_user.should_not be_valid
+    end
+  end
 end
