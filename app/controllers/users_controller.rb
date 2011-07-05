e058371a8a4afc909191c26dbb7632634bad1569
brook <brook_realize@126.com>
2011-07-05 18:03:39 +0800

    relationship 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
-  before_filter :authenticate , :only=>[:edit , :update , :index]
+  before_filter :authenticate , :except => [:show , :new ,:create]
+  #before_filter :authenticate , :only=>[:edit , :update , :index]
   before_filter :correct_user , :only=>[:edit , :update]
   before_filter :admin_user , :only=>:destroy
   before_filter :unlogged_in_user , :only=>[:new,:create]
+  
   
   def index
     @title = "All users"
@@ -53,6 +55,20 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User destroyed."
     redirect_to users_path
+  end
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+  
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page=> params[:page])
+    render 'show_follow'
   end
   
   
