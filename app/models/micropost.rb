e995fabff6ae84bc7177afaaa2c5926b778ccbe8
brook <brook_realize@126.com>
2011-07-06 11:43:05 +0800

    added user following, but ajax not work

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -8,4 +8,17 @@ class Micropost < ActiveRecord::Base
   
   
   default_scope :order =>'microposts.created_at DESC'
+  
+  scope :from_users_followed_by, lambda {|user| followed_by(user)}
+  
+  
+  
+  
+  
+  private ########################################################
+  def self.followed_by(user)
+    followed_ids = %(select followed_id from [relationships] where follower_id = :user_id)
+    where("user_id in (#{followed_ids}) or user_id= :user_id",{:user_id=> user})
+  end
+  
 end
